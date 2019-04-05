3e9baa607b3839e920ba89a46bba32d234b8fa02
Andrew Shough <andrewshough@gmail.com>
2019-04-05 08:47:27 +0200

    example tests.

---
+++ Custom-identity-Schema/TestingExample.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Custom_identity_Schema
+{
+    public class TestingExample
+    {
+        public Guid IdGen()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,14 +1,19 @@
 using System;
+using Custom_identity_Schema;
 using Xunit;
 
 namespace tests
 {
-    public class UnitTest1
+    public class IdGenTests
     {
         [Fact]
-        public void Test1()
+        public void NewIdShouldReturnUniqueValues()
         {
+            var idGen = new TestingExample();
+            var id1 = idGen.IdGen();
+            var id2 = idGen.IdGen();
 
+            Assert.NotEqual(id1, id2);
         }
     }
 }
